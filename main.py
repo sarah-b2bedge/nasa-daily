5364812064bf9d00dc45780357da17e6e759f390
sarah <sarah@b2bedge.com>
2026-03-22 13:00:46 +0800

    Initial commit

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import streamlit as st
 import requests
 
+# Prepare API key and API ur
 api_key = "hqGbT3TOzsGWXeYmS7znqVfsYXz9gzH79ifcgqtk"
 url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
 
+# Get the request data as a dictionary
 response = requests.get(url)
 content = response.json()
 
-st.title("Galaxy by the lake")
+# Extract the title, url, and explanation from the content and display them in the Streamlit app
+st.title(content["title"])
 st.video(content["url"])
 st.text(content["explanation"])
