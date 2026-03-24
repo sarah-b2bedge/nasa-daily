56963bbb52d843b82621cbceb961ef8d510b4fa9
sarah <sarah@b2bedge.com>
2026-03-24 22:01:18 +0800

    Implemented dotenv

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import streamlit as st
 import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Prepare API key and API ur
-api_key = "hqGbT3TOzsGWXeYmS7znqVfsYXz9gzH79ifcgqtk"
-url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
+NASA_API_KEY = os.getenv("NASA_API_KEY")
+url = f"https://api.nasa.gov/planetary/apod?api_key={NASA_API_KEY}"
 
 # Get the request data as a dictionary
 response = requests.get(url)
@@ -11,5 +15,8 @@ content = response.json()
 
 # Extract the title, url, and explanation from the content and display them in the Streamlit app
 st.title(content["title"])
-st.video(content["url"])
+if content["media_type"] == "video":
+    st.video(content["url"])
+else:
+    st.image(content["url"])
 st.text(content["explanation"])
